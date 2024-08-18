1e25c063da0ff840c83bebdbbbd1e38cf930d728
hiroro-work <hiroro@sonicgarden.jp>
2024-08-18 20:08:19 +0900

    Fixes to avoid errors if the size of the refs/queries between renders.

--- src/firestore/useQueriesEffect.ts
+++ src/firestore/useQueriesEffect.ts
@@ -4,12 +4,12 @@ import type { CustomCompareFunction } from '../utils/index.js';
 import type { Query } from 'firebase/firestore';
 import type { EffectCallback } from 'react';
 
-const queriesEqual = <T>(prevQueries: Query<T>[], queries: Query<T>[]) => {
+const queriesEqual = <T>([prevQueries]: Query<T>[][], [queries]: Query<T>[][]) => {
   if (prevQueries.length !== queries.length) return false;
 
   return prevQueries.every((prev, i) => queryEqual(prev, queries[i] as Query<T>));
 };
 
 export const useQueriesEffect = <T>(effect: EffectCallback, queries: Query<T>[]) => {
-  useCustomCompareEffect(effect, queries, queriesEqual as CustomCompareFunction);
+  useCustomCompareEffect(effect, [queries], queriesEqual as CustomCompareFunction);
 };
